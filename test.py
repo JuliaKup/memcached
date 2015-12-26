fde54f92ae9f5089d8f14f61ed56ebccd1101ff1
Julia <yuliya.wolf@gmail.com>
2015-12-26 09:50:27 +0300

    Uploded test.py

--- test.py
+++ test.py
@@ -1,39 +1,53 @@
 import socket
 import sys
 import subprocess
+from multiprocessing import Pool
+import time
 
 port = 54322
 
-def check_echo(msg, exp_res):
+def connect(port, msg):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("localhost", int(port)))
 
     s.send(msg)
     
-    res = s.recv(len(exp_res))
+    res = s.recv(4096)
     print(res)
-    assert exp_res == res
+    return res
 
 if __name__ == "__main__":
 	port = sys.argv[1]
 	mc = subprocess.Popen(['./server', port])
-
-	#mcg = "get abc\r\n"
-	#exp_res = "NOT_FOUND"
-	#check_echo(mcg, exp_res)
-
-	mcg = "set abc 1 1\r\n5\r\n"
+	time.sleep(1)
+	mcg = "add abc 1 0 1\r\n5\r\n"
 	exp_res = "STORED"
-	check_echo(mcg, exp_res)
-	print(1)
+	pool = Pool(processes = 10)              # start 100 worker processes
+	result = pool.apply_async(connect, [port, mcg], callback=lambda x: x == exp_res)    # evaluate asynchronously
+	result.wait()
 
 	mcg = "get abc\r\n"
-	exp_res = "VALUE abc 0 1\r\n5\r\n"
-	check_echo(mcg, exp_res)
-	print(3)
+	exp_res = "VALUE abc 1 1\r\n5\r\nEND"
+	pool = Pool(processes = 10)              # start 100 worker processes
+	result = pool.apply_async(connect, [port, mcg], callback=lambda x: x == exp_res)    # evaluate asynchronously
+	result.wait()
+
+	mcg = "set abc 1 0 2\r\n55\r\n"
+	exp_res = "STORED"
+	pool = Pool(processes = 10)              # start 100 worker processes
+	result = pool.apply_async(connect, [port, mcg], callback=lambda x: x == exp_res)    # evaluate asynchronously
+	result.wait()
+
+	mcg = "get abc\r\n"
+	exp_res = "VALUE abc 1 2\r\n55\r\nEND"
+	pool = Pool(processes = 10)              # start 100 worker processes
+	result = pool.apply_async(connect, [port, mcg], callback=lambda x: x == exp_res)    # evaluate asynchronously
+	result.wait()
 
 	mcg = "delete abc\r\n"
 	exp_res = "DELETED"
-	check_echo(mcg, exp_res)
+	pool = Pool(processes = 10)              # start 100 worker processes
+	result = pool.apply_async(connect, [port, mcg], callback=lambda x: x == exp_res)    # evaluate asynchronously
+	result.wait()
 
-	mc.kill()
+	mc.kill
